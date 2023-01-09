012713ec9a3e93e183d2726a8a980fc371523fa7
NicolasLOrtiz <contato.nicolol@gmail.com>
2023-01-09 20:54:50 -0300

    feat: Melhoria no menu

--- src/main.js
+++ src/main.js
@@ -22,67 +22,67 @@ document.querySelector('#app').innerHTML = `
   <h1>
   Buttons
 </h1>
-<div class="buttons">
-<a href="/components/buttons/magic-line-fill/">Magic Line Fill</a>
-<a href="/components/buttons/ellipsis-hover-effect/">Ellipsis Hover Effect</a>
-<a href="/components/buttons/advanced-css-button-hover-animation-effects/">Advanced CSS Button Hover Animation Effects</a>
-</div>
+<ul class="buttons">
+<li><a href="/components/buttons/magic-line-fill/">Magic Line Fill</a></li>
+<li><a href="/components/buttons/ellipsis-hover-effect/">Ellipsis Hover Effect</a></li>
+<li><a href="/components/buttons/advanced-css-button-hover-animation-effects/">Advanced CSS Button Hover Animation Effects</a></li>
+</ul>
 
 <h1>Cards</h1>
-<div class="cards">
-<a href="/components/cards/dark-neumorphism/">Dark neumorphism</a>
-<a href="/components/cards/ProductCard_Descr/">Product Card with description</a>
-<a href="/components/cards/ProductCard_Detail/">Product Card with details</a>
-<a href="/components/cards/ProductCard_UI/">Product Card with new UI</a>
-</div>
+<ul class="cards">
+<li><a href="/components/cards/dark-neumorphism/">Dark neumorphism</a></li>
+<li><a href="/components/cards/ProductCard_Descr/">Product Card with description</a></li>
+<li><a href="/components/cards/ProductCard_Detail/">Product Card with details</a></li>
+<li><a href="/components/cards/ProductCard_UI/">Product Card with new UI</a></li>
+</ul>
 
-  <h1>
+<h1>
   Forms
 </h1>
 <h3>Autentication</h3>
-<div class="buttons">
-<a href="/components/forms/autentication/responsive-vanilla-slide/">Responsive Vanilla Slide</a>
-</div>
+<ul class="buttons">
+<li><a href="/components/forms/autentication/responsive-vanilla-slide/">Responsive Vanilla Slide</a></li>
+</ul>
 
 <h3>Contact</h3>
-<div class="buttons">
-<a href="/components/forms/contact/responsive-floating-placeholder/">Responsive Floating PlaceHolder</a>
-</div>
+<ul class="buttons">
+<li><a href="/components/forms/contact/responsive-floating-placeholder/">Responsive Floating PlaceHolder</a></li>
+</ul>
 
 <h1>
   Menus
 </h1>
-<div class="buttons">
-<a href="/components/menus/dashboard-sidebar-menu/">Dashboard Sidebar Menu</a>
-</div>
+<ul class="buttons">
+<li><a href="/components/menus/dashboard-sidebar-menu/">Dashboard Sidebar Menu</a></li>
+</ul>
 
 <h1>
   Loader
 </h1>
-<div class="buttons">
-<a href="/components/loader/neumorphism-gradient-loader-ring-animation/">Neumorphism Gradient Loader Ring Animation</a>
-<a href="/components/loader/text-typing-loading-animation/">Text Typing Loading Animation</a>
-</div>
+<ul class="buttons">
+<li><a href="/components/loader/neumorphism-gradient-loader-ring-animation/">Neumorphism Gradient Loader Ring Animation</a></li>
+<li><a href="/components/loader/text-typing-loading-animation/">Text Typing Loading Animation</a></li>
+</ul>
 
 <h1>
   Pages
 </h1>
-<div class="buttons">
-<a href="/pages/css-cv/">CSS CV</a>
-<a href="/pages/css-running-animation/">CSS Running Animation</a>
-<a href="/pages/html-css-js-portifolio/">HTML/CSS/JS Portifolio</a>
-<a href="/pages/html-meaningful-html/">Meaningful HTML</a>
-<a href="/pages/html-responsive-fallback/">HTML Responsive Fallback</a>
-<a href="/pages/html-responsive-image/">HTML Responsive Image</a>
-<a href="/pages/html-responsive-picture/">HTML Responsive Picture</a>
-<a href="/pages/html-sss/">HTML SSS</a>
-<a href="/pages/js-analog-clock/">JS Analog Clock</a>
-<a href="/pages/js-responsive-resolution/">JS Responsive Resolution</a>
-<a href="/pages/js-speed-test/">JS Speed Test</a>
-<a href="/pages/site-simples-responsivo/">Responsive</a>
-<a href="/pages/starbucks-landing-page/">Starbucks Landing Page</a>
-<a href="/pages/woodland/">Woodland</a>
-</div>
+<ul class="buttons">
+<li><a href="/pages/css-cv/">CSS CV</a></li>
+<li><a href="/pages/css-running-animation/">CSS Running Animation</a></li>
+<li><a href="/pages/html-css-js-portifolio/">HTML/CSS/JS Portifolio</a></li>
+<li><a href="/pages/html-meaningful-html/">Meaningful HTML</a></li>
+<li><a href="/pages/html-responsive-fallback/">HTML Responsive Fallback</a></li>
+<li><a href="/pages/html-responsive-image/">HTML Responsive Image</a></li>
+<li><a href="/pages/html-responsive-picture/">HTML Responsive Picture</a></li>
+<li><a href="/pages/html-sss/">HTML SSS</a></li>
+<li><a href="/pages/js-analog-clock/">JS Analog Clock</a></li>
+<li><a href="/pages/js-responsive-resolution/">JS Responsive Resolution</a></li>
+<li><a href="/pages/js-speed-test/">JS Speed Test</a></li>
+<li><a href="/pages/site-simples-responsivo/">Responsive</a></li>
+<li><a href="/pages/starbucks-landing-page/">Starbucks Landing Page</a></li>
+<li><a href="/pages/woodland/">Woodland</a></li>
+</ul>
 `
 
 setupCounter(document.querySelector('#counter'))
